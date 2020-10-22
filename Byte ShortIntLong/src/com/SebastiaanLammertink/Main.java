3857242c4580a87a9744771cf5439ec0300d6f67
SLammertink <Bea991213!>
2020-10-22 23:31:06 +0400

    Late evening commit

--- Byte ShortIntLong/src/com/SebastiaanLammertink/Main.java
+++ Byte ShortIntLong/src/com/SebastiaanLammertink/Main.java
@@ -11,5 +11,30 @@ public class Main {
         System.out.println("Integer Maximum value = " + myMaxIntValue);
         System.out.println("Busted Integer Minimum Value: = " + (myMinIntValue - 1));
         System.out.println("Busted Integer Maximum Value = " + (myMaxIntValue + 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        Short myMinShortValue = Short.MIN_VALUE;
+        Short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        Long myMinLongValue = Long.MIN_VALUE;
+        Long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long myLongValue = 100L;
+        long bigLongLiteralValue = 2_147_483_647L;
+        System.out.println(bigLongLiteralValue);
+
+        int myTotal = (myMinIntValue / 2);
+        byte myNewByteValue = (byte)( myMinByteValue / 2);
+        short myNewShortValue = (short) (myMinShortValue / 2);
     }
 }
